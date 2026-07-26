d69f8a685dd811efa3e648b8035cbba324bc833e
Yuhithan <yuhithans@gmail.com>
2026-07-26 08:02:13 +0000

    change update

--- OS/src/impl/kernel/main.c
+++ OS/src/impl/kernel/main.c
@@ -6,8 +6,8 @@ void kmain() {
     enable_key_input();
     imp_cls();
     imp_color(PRINT_COLOR_BLACK, PRINT_COLOR_WHITE);
-    imp_text("==========================================\n");
-    imp_text("Welcome to ORT, the new rival of Windows\n");
+    imp_text("===========================================\n");
+    imp_text("Welcome to Orange Tea OS!\n");
     imp_text("==========================================\n");
 
     shell_init();

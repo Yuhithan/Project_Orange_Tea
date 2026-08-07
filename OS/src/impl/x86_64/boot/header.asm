4bd49845044899f502349dc79280a3064682ac5e
Yuhithan <yuhithans@gmail.com>
2026-08-07 18:54:12 +0000

    test4

--- OS/src/impl/x86_64/boot/header.asm
+++ OS/src/impl/x86_64/boot/header.asm
@@ -1,14 +1,20 @@
+section .multiboot_header
+align 8
+
 header_start:
-    ;MAGIC
+    ; Multiboot2 magic
     dd 0xe85250d6
 
+    ; Architecture 0 (protected mode i386)
     dd 0
 
+    ; Header length
     dd header_end - header_start
 
+    ; Checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-    ; framebuffer request tag
+    ; Framebuffer request tag (type 5)
     dw 5
     dw 0
     dd 20
@@ -16,8 +22,10 @@ header_start:
     dd 768
     dd 32
 
-    ; end tag
+    ; End tag
+    align 8
     dw 0
     dw 0
     dd 8
+
 header_end:

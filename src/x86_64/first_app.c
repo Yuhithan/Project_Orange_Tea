d22654c00de069a9fd3ec8917d242782c39268a8
Yuhithan <yuhithans@gmail.com>
2026-08-26 16:29:52 +0000

    return 1

--- src/x86_64/first_app.c
+++ src/x86_64/first_app.c
@@ -8,10 +8,9 @@ static void welcome_draw(ORWindow *window)
 {
     int x = window->x + 18;
     int y = window->y + 39;
-    ORgui_draw_text(x, y, "ENVIRONEMENT GRAPHIQUE DE ORTOS", OR_COLOR_TEXT);
-    ORgui_draw_text(x, y + 18, "APPLICATION DE ORGUI(LIB)", OR_COLOR_FIRE_YELLOW);
-    ORgui_draw_text(x, y + 36, "BUREAU BARE METAL", OR_COLOR_TEXT_DIM);
-    //ORgui_draw_text(x, y + 54, "SHOUT OUT POUR (@DM)DEV_TOOLS1", OR_COLOR_TEXT);
+    ORgui_draw_text(x, y, "ORTOS GRAPHICAL ENVIRONMENT", OR_COLOR_TEXT);
+    ORgui_draw_text(x, y + 18, "POWERED BY ORGUI", OR_COLOR_FIRE_YELLOW);
+    ORgui_draw_text(x, y + 36, "BARE METAL DESKTOP", OR_COLOR_TEXT_DIM);
     ORgui_draw_button(x, y + 60, 94, 22, "CLICK ME", clicked);
     ORgui_draw_text(x, y + 91, clicked ? "THE FIRE IS LIT" : "PRESS ENTER", OR_COLOR_FIRE_ORANGE);
 }
@@ -27,7 +26,7 @@ static void welcome_event(ORWindow *window, const OREvent *event)
 
 void first_app_init(void)
 {
-    welcome_window = ORgui_create_window(0, 0, 310, 155, "BIENVENUE SUR MON OS");
+    welcome_window = ORgui_create_window(0, 0, 310, 155, "WELCOME TO ORTOS");
     if (welcome_window == 0) return;
     welcome_window->on_draw = welcome_draw;
     welcome_window->on_event = welcome_event;

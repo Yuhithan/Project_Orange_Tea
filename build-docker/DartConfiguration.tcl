431d8cb80bd3b6c5e9c9a7b1faead7d236f21c7b
Lightbowlt <yuhithans@outlook.com>
2026-08-27 17:32:01 +0000

    v3.0.5-fix

--- build-docker/DartConfiguration.tcl
+++ build-docker/DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: /root/env/build-docker
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: 9d4044d10767
+Site: 90302df6a265
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Linux-unknown

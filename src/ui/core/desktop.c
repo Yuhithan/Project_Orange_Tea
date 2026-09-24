e8f000cd177d8e9f30fd91af9c890cd22f82d781
Yuhithan <yuhithans@gmail.com>
2026-09-24 10:00:21 +0000

    v.3.2.0-OS

--- src/ui/core/desktop.c
+++ src/ui/core/desktop.c
@@ -37,7 +37,7 @@ void desktop_draw(void)
     fb_clear(OR_COLOR_BACKGROUND);
     fb_fill_rect(0, 0, fb_width(), 34, OR_COLOR_PANEL);
     fb_draw_line(0, 33, fb_width() - 1, 33, OR_COLOR_FIRE_RED);
-    ORgui_draw_text(14, 14, "ORTOS DESKTOP - ALPHA-3.0.0", OR_COLOR_FIRE_YELLOW);
+    ORgui_draw_text(14, 14, "ORTOS DESKTOP - ALPHA-3.2.0", OR_COLOR_FIRE_YELLOW);
     ORgui_draw_text(190, 14, "WILDFIRE", OR_COLOR_FIRE_ORANGE);
     ORgui_draw();
     taskbar_draw();
